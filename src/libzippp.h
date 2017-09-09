bbc2b126c67a212d37172d31ab1e0dc9b881a4d2
Cédric Tabin <cedric.tabin@gmail.com>
2017-09-09 23:18:39 +0200

    minor documentation fix

--- src/libzippp.h
+++ src/libzippp.h
@@ -503,7 +503,7 @@ namespace libzippp {
          * doesn't belong to the archive, LIBZIPPP_ERROR_FOPEN_FAILURE if zip_fopen_index() has failed, LIBZIPPP_ERROR_MEMORY_ALLOCATION if 
          * a memory allocation has failed, LIBZIPPP_ERROR_FREAD_FAILURE if zip_fread() didn't succeed to read data, 
          * LIBZIPPP_ERROR_OWRITE_INDEX_FAILURE if the last ofstream operation has failed, LIBZIPPP_ERROR_OWRITE_FAILURE if fread() didn't 
-         * return the exact amount of requested bytes and -9 if the amount of extracted bytes didn't match the size of the file (unknown error).
+         * return the exact amount of requested bytes and LIBZIPPP_ERROR_UNKNOWN if the amount of extracted bytes didn't match the size of the file (unknown error).
          * If the provided chunk size is zero, it will be defaulted to DEFAULT_CHUNK_SIZE (512KB).
          * The method doesn't close the ofstream after the extraction.
          */
