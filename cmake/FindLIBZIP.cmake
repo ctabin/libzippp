ccb7d4bfd28f25ef988178ff4f0a33d455392053
Flamefire <Flamefire@users.noreply.github.com>
2019-11-17 18:08:26 +0100

    Enhance CMake to allow use as submodule
    Prefix config vars with project name
    Allow disabling of tests and install
    Minor refactoring
    Fixes #51

--- cmake/FindLIBZIP.cmake
+++ cmake/FindLIBZIP.cmake
@@ -7,7 +7,7 @@ mark_as_advanced(LIBZIP_INCLUDE_DIR)
 find_library(LIBZIP_LIBRARY NAMES zip)
 mark_as_advanced(LIBZIP_LIBRARY)
 
-include(CMakeFindDependencyMacro)
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     LIBZIP 
     REQUIRED_VARS
@@ -16,13 +16,11 @@ find_package_handle_standard_args(
 )
 
 if (LIBZIP_FOUND)
-    set(LIBZIP_INCLUDE_DIRS "${LIBZIP_INCLUDE_DIR}")
-
     if (NOT TARGET libzip::libzip)
         add_library(libzip::libzip UNKNOWN IMPORTED)
         set_target_properties(libzip::libzip
             PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES ${LIBZIP_INCLUDE_DIRS}
+                INTERFACE_INCLUDE_DIRECTORIES ${LIBZIP_INCLUDE_DIR}
                 INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB;BZip2::BZip2"
                 IMPORTED_LOCATION "${LIBZIP_LIBRARY}"
         )
